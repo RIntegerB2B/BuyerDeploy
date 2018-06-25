75d0e49d5f03b93dfad9d561ed07c06f2a965fbf
Chandran <Preveen_Chandran@Dell.com>
2018-06-25 20:50:28 +0530

    Timeout Increased

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors());
 app.listen(port);
-
+app.timeout = 300000;  
 
 
 app.get('/test', function (req, res) {
